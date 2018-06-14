1e4dc17a8b24717f9b98924f7211c42b7dd8e29a
MyriamFentanes <myriam.fentanes@gmail.com>
2018-06-14 13:34:34 -0500

    Update allinone.sh

--- allinone.sh
+++ allinone.sh
@@ -98,3 +98,6 @@ EOF
 ansible-playbook -i /etc/ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i /etc/ansible/hosts /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
 htpasswd -c -b /etc/origin/master/htpasswd ${AUSERNAME} ${PASSWORD}
+oc login -u ${AUSERNAME} -p ${PASSWORD} 
+oc new-project webappTest
+oc project webappTest
